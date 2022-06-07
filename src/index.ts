441bec3106fed7cdce3b089a4f761e973c14ba86
김종한 <jong@BlackStrawberry.local>
2022-06-08 00:54:30 +0900

    type chain has an error and next time I will fix all

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import {init, exit} from "./myPackage";
-
-init({
-  url: "true",
-  debug: false,
-});
-
-exit(1);
+import * as crypto from "crypto";
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BlockShape{
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ){
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash:string, height:number, data:string){}
+}
